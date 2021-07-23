1ac91e01ce4cdbe117460d799d6eb43e1aa569c5
Ari Rhel <arirhel@pm.me>
2021-07-23 15:19:18 -0500

    First commit

---
+++ src/main/java/com/arirhel/thisorthat/ThisOrThatApplication.java
@@ -0,0 +1,13 @@
+package com.arirhel.thisorthat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisOrThatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisOrThatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arirhel/thisorthat/ThisOrThatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arirhel.thisorthat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisOrThatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
