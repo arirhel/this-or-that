c2de5a17ede4f24421d2a76eef496147f6eeb8c3
Ari Rhel <arirhel@pm.me>
2021-07-26 16:45:54 -0500

    Add command line runner

--- src/main/java/com/arirhel/thisorthat/ThisOrThatApplication.java
+++ src/main/java/com/arirhel/thisorthat/ThisOrThatApplication.java
@@ -1,13 +1,25 @@
 package com.arirhel.thisorthat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThisOrThatApplication {
+// todo when adding frontend, remove application.properties:spring.main.web-application-type=NONE
+public class ThisOrThatApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThisOrThatApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(ThisOrThatApplication.class);
 
+    public static void main(String[] args) {
+        log.info("Starting ThisOrThatApplication");
+        SpringApplication.run(ThisOrThatApplication.class, args);
+        log.info("Application finished");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Running ThisOrThatApplication");
+    }
 }
