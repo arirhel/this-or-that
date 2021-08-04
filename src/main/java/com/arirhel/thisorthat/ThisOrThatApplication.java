f32ec3e9a5acb01b1d1eff5af736dc370d66d184
Ari Rhel <arirhel@pm.me>
2021-08-04 14:38:35 -0500

    Remove shuffle service

--- src/main/java/com/arirhel/thisorthat/ThisOrThatApplication.java
+++ src/main/java/com/arirhel/thisorthat/ThisOrThatApplication.java
@@ -1,38 +1,13 @@
 package com.arirhel.thisorthat;
 
-import com.arirhel.thisorthat.service.ShuffleService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
-// todo when adding frontend, remove application.properties:spring.main.web-application-type=NONE
-public class ThisOrThatApplication implements CommandLineRunner {
-
-    private static final Logger log = LoggerFactory.getLogger(ThisOrThatApplication.class);
-
-    @Autowired
-    ShuffleService shuffleService;
+public class ThisOrThatApplication {
 
     public static void main(String[] args) {
-        log.info("Starting ThisOrThatApplication");
         SpringApplication.run(ThisOrThatApplication.class, args);
-        log.info("Application finished");
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("Running ThisOrThatApplication with default dataset: Choice Cuisines");
-        final List<String> cuisines = Arrays.asList("Italian", "Thai", "French", "Japanese", "Lebanese", "Spanish",
-                "German", "Korean", "West African", "Australian", "Caribbean", "Greek", "Filipino", "Vietnamese",
-                "Indian", "Mexican", "Indonesian", "Brazilian", "Chinese", "American");
-        shuffleService.shuffle(cuisines);
-        log.info(cuisines.toString());
-    }
 }
--- src/main/java/com/arirhel/thisorthat/service/ShuffleService.java
+++
@@ -1,7 +0,0 @@
-package com.arirhel.thisorthat.service;
-
-import java.util.List;
-
-public interface ShuffleService {
-    List<String> shuffle(List<String> list);
-}
