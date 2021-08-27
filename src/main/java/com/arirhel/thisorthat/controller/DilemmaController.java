f5e4e8eba5345a3eb8df7aaf34f2491ab36609b5
Ari Rhel <arirhel@pm.me>
2021-08-27 11:11:42 -0500

    Add dilemma detail controller and view

--- src/main/java/com/arirhel/thisorthat/controller/DilemmaController.java
+++ src/main/java/com/arirhel/thisorthat/controller/DilemmaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,8 +25,14 @@ public class DilemmaController {
         this.dilemmaService = dilemmaService;
     }
 
+    @PostMapping("/save")
+    public ModelAndView save(@ModelAttribute Dilemma form) {
+        System.out.println(form.toString());
+        return detail(form.getId()); // temporary
+    }
+
     @GetMapping("/list")
-    public ModelAndView dilemmaList(
+    public ModelAndView list(
       @RequestParam(name = "page", defaultValue = "0") int page,
       @RequestParam(name = "size", defaultValue = "10") int size) {
         final ModelAndView modelAndView = new ModelAndView("dilemma/list");
@@ -33,6 +41,23 @@ public class DilemmaController {
         modelAndView.addObject("currentPage", dilemmaPage.getPageable().getPageNumber());
         modelAndView.addObject("size", dilemmaPage.getSize());
         modelAndView.addObject("ofPages", dilemmaPage.getTotalPages() - 1); // Offset for zero page
+        return modelAndView;
+    }
+
+    @GetMapping("/detail")
+    public ModelAndView detail(@RequestParam(name = "id") String id) {
+        final ModelAndView modelAndView = new ModelAndView();
+        final Optional<Dilemma> optionalDilemma = dilemmaService.findById(id);
+        if (optionalDilemma.isPresent()) {
+            modelAndView.setViewName("dilemma/detail");
+            modelAndView.addObject("form", optionalDilemma.get());
+            modelAndView.addObject("id", optionalDilemma.get().getId());
+            modelAndView.addObject("question", optionalDilemma.get().getQuestion());
+            modelAndView.addObject("candidates", optionalDilemma.get().getCandidates());
+        } else {
+            modelAndView.setViewName("dilemma/notfound");
+            modelAndView.addObject("id", id);
+        }
         return modelAndView;
     }
 
